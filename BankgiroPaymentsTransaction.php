b7ba2a05b5a343f4f33b9bb0d6ce27ec31bcb1d7
Daniel Josefsson <dannejosefsson@gmail.com>
2012-03-26 14:18:41 +0200

    Functions for parsing transactions here is written.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- BankgiroPaymentsTransaction.php
+++ BankgiroPaymentsTransaction.php
@@ -10,7 +10,7 @@
 * @author		Daniel Josefsson <dannejosefsson@gmail.com>
 * @version		v0.1
 */
-abstract class BankgiroPaymentsTransaction
+class BankgiroPaymentsTransaction
 {
 	/**
 	* Transaction code.
@@ -85,25 +85,25 @@ abstract class BankgiroPaymentsTransaction
 	/**
 	* Name of the sender
 	* Two strings of names is stored together in this string.
-	* Length of strings: 35
+	* Max length of strings: 35
 	* Max total Length: 70
 	* @var string
 	*/
-	protected	$_name;
+	protected	$_names;
 	/**
 	* Adress1 field stores address and postal number
-	* Postal number length: 9
-	* Address length: 35
-	* @var string
+	* Address max length: 35
+	* Postal number max length: 9
+	* @var array
 	*/
 	protected	$_address1;
 	/**
 	* Adress2 field stores city, country and countrycode
-	* City length: 35
-	* Country length: 35
-	* Country code length: 2
+	* City max length: 35
+	* Country max length: 35
+	* Country max code length: 2
 	* Country and country code are blank if it is a domestic transaction.
-	* @var string
+	* @var array
 	*/
 	protected	$_address2;
 	/**
@@ -112,6 +112,22 @@ abstract class BankgiroPaymentsTransaction
 	* @var int
 	*/
 	protected	$_organisationNumber;
+
+	/**
+	* Array with extra references
+	* @var array of BankgiroPaymentTransaction
+	 */
+	protected	$_extraReferences;
+
+	/**
+	*
+	* @since	v0.2
+	*/
+	public function __construct()
+	{
+		$this->_names = array();
+		$this->_extraReferences = array();
+	}
 
 	/**
 	* Returns transaction code.
@@ -339,7 +355,7 @@ abstract class BankgiroPaymentsTransaction
 	*/
 	public function setInformation( $information )
 	{
-		$this->_information = $information;
+		$this->_information[] = $information;
 		return $this;
 	}
 
@@ -348,9 +364,9 @@ abstract class BankgiroPaymentsTransaction
 	* @author	Daniel Josefsson
 	* @since	v0.1
 	*/
-	public function getName()
+	public function getNames( $index )
 	{
-		return $this->_name;
+		return $this->_names[$index];
 	}
 
 	/**
@@ -360,9 +376,9 @@ abstract class BankgiroPaymentsTransaction
 	* @param	int $name
 	* @return	BankgiroPaymentsTransaction
 	*/
-	public function setName( $name )
+	public function setNames( $names = array() )
 	{
-		$this->_name = $name;
+		$this->_names[] = $names;
 		return $this;
 	}
 
@@ -435,4 +451,218 @@ abstract class BankgiroPaymentsTransaction
 		return $this;
 	}
 
+	/**
+	* Returns extra references.
+	* @author	Daniel Josefsson
+	* @since	v0.2
+	* @return	array of BankgiroPaymentsTransaction
+	*/
+	public function getExtraReferences()
+	{
+		return $this->_extraReferences;
+	}
+
+	/**
+	* Sets new extra reference.
+	* @author	Daniel Josefsson
+	* @since	v0.2
+	* @param	BankgiroPaymentsTransaction $extraReference
+	* @return	BankgiroPaymentsTransaction $this
+	*/
+	public function setExtraReference( BankgiroPaymentsTransaction $extraReference )
+	{
+		$this->_extraReferences[] = $extraReference;
+		return $this;
+	}
+
+	/**
+	* Return index to the latest inserted extra reference.
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @return	int $keyToExtraReference
+	*/
+	public function lastExtraReferenceIndex()
+	{
+		end($this->_extraReferences);
+		return key($this->_extraReferences);
+	}
+
+	/**
+	* Returns wanted extra reference
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	int $index
+	* @return	BankgiroPaymentsTransaction $extraReference
+	*/
+	public function getExtraReference( $index )
+	{
+		return $this->_extraReferences[$index];
+	}
+
+	/**
+	* Add new extra reference.
+	* Increments reference count.
+	* @author	Daniel Josefsson
+	* @since	v0.2
+	* @param	string 						$postType
+	* @return	BankgiroPaymentsTransaction $this
+	*/
+	public function addExtraReference($postType)
+	{
+		$this->_extraReferences[] = new BankgiroPaymentsTransaction();
+
+		return $this;
+	}
+
+	/**
+	* Return the extra references count.
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @return	int
+	*/
+	public function getExtraReferencesCount()
+	{
+		return sizeof($this->_extraReferences);
+	}
+
+	/**
+	* Parses line,given with the syntax of posts 20 to 29 in Bankgiro Inbetalningar.
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param	string $line
+	* @return	boolean
+	*/
+	public function parsePost( $line )
+	{
+		$return = false;
+		$postType = substr($line, 0, 2);
+		$postData = substr($line, 2);
+		switch ($postType)
+		{
+			case '20':
+				$return = $this->parsePaymentPost($line);
+				break;
+			case '21':
+				$return = $this->parseDeductionPost($line);
+				break;
+			case '22':
+			case '23':
+				$extraReferenceIndex = $this->addExtraReference($postType)->lastExtraReferenceIndex();
+				$return = $this->getExtraReference($extraReferenceIndex)->parsePaymentPost($line);
+
+				break;
+			case '25':
+				$return = $this->parseInformationPost($postData);
+				break;
+			case '26':
+				$return = $this->parseNamePost($postData);
+				break;
+			case '27':
+				$return = $this->parseAddress1Post($postData);
+				break;
+			case '28':
+				$return = $this->parseAddress2Post($postData);
+				break;
+
+			default:
+				break;
+		}
+		return $return;
+	}
+
+	/**
+	* Parses payment post (20).
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	string	$post
+	* @return	bool
+	*/
+	private function parsePaymentPost( $post )
+	{
+		$this->setTransactionCode(substr($post, 0, 2));
+		$this->setSenderBankgiroAccountNumber((int) substr($post, 2, 10));
+		$this->setReferenceCode((int) substr($post, 55, 1));
+		$this->setReference(trim(substr($post, 12, 25), ' '));
+		$this->setAmount((int) substr($post, 37, 18));
+		$this->setChannelCode((int) substr($post, 56, 1));
+		$this->setSerialNumber(substr($post, 57, 12));
+		$this->setImageMarker((int) substr($post, 69, 1));
+		// Reserved placeholders (post[70-79]) are not used.
+		return true;
+	}
+
+	/**
+	* Parses deduction post (21).
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	string				$post
+	* @return	bool
+	*/
+	private function parseDeductionPost( $post )
+	{
+		$this->parsePaymentPost($post);
+		$this->setDeductionCode((int) substr($post, 70, 1));
+		// Reserved placeholders (post[70-79]) are not used.
+		return true;
+	}
+
+	/**
+	* Parses information post (25).
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	string	$postData
+	* @return	bool
+	*/
+	private function parseInformationPost( $postData )
+	{
+		$this->setInformation(trim(substr($postData, 0, 50), ' '));
+		// Reserved placeholders (postData[50-77]) are not used.
+		return true;
+	}
+
+	/**
+	* Parses name post (26).
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	string	$postData
+	* @return	bool
+	*/
+	private function parseNamePost( $postData )
+	{
+		$this->setNames(array(	trim(substr($postData, 0, 35), ' '),
+								trim(substr($postData, 35, 35), ' ')));
+		// Reserved placeholders (postData[70-77]) are not used.
+		return true;
+	}
+
+	/**
+	* Parses first address post (27).
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	string	$postData
+	* @return	bool
+	*/
+	private function parseAddress1Post( $postData )
+	{
+		$this->setAddress1(array(	trim(substr($postData, 0, 35), ' '),
+										trim(substr($postData, 35, 9), ' ')));
+		// Reserved placeholders (postData[44-77]) are not used.
+		return true;
+	}
+
+	/**
+	* Parses second address post (28).
+	* @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	* @since	v0.2
+	* @param 	string	$postData
+	* @return	bool
+	*/
+	private function parseAddress2Post( $postData )
+	{
+		$this->setAddress2(array(	trim(substr($postData, 0, 35), ' '),
+										trim(substr($postData, 35, 35), ' '),
+										trim(substr($postData, 70, 2)))		);
+		// Reserved placeholders (postData[72-77]) are not used.
+		return true;
+	}
 }
