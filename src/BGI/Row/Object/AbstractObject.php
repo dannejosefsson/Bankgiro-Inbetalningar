89943f03d293b2e400c29d30f41cdda4a84cf1d9
Daniel Josefsson <dannejosefsson@gmail.com>
2012-10-10 23:31:58 +0200

    Added comments and some basic functions.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- src/BGI/Row/Object/AbstractObject.php
+++ src/BGI/Row/Object/AbstractObject.php
@@ -1,17 +1,76 @@
 <?php
 namespace BGI\Row\Object;
 
+/**
+ * An object defines the data that is transferred between a
+ * BGI\Row\Parser.ParserInterface and an
+ * BGI\InfoContainer.InfoContainerInterface.
+ * @author Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 abstract class AbstractObject implements ObjectInterface
 {
+	/**
+	 * Must be string if transaction code is below 10 and will then be zero
+	 * filled, otherwise it can be an int.
+	 * @access	protected
+	 * @var		string|int
+	 */
+	protected $_transactionCode;
+
 	/**
 	 * (non-PHPdoc)
 	 * @see		BGI\Row\Object.ObjectInterface::__construct(array $data)
 	 * @param	array $data
 	 */
-	abstract public function __construct(array $data);
+	public function __construct(array $data = array())
+	{
+		$this->setData($data);
+	}
+
 	/**
 	 * (non-PHPdoc)
 	 * @see BGI\Row\Object.ObjectInterface::setData()
 	 */
 	abstract public function setData(array $data);
+
+	/**
+	 * (non-PHPdoc)
+	 * @see BGI\Row\Object.ObjectInterface::getTransactionCode()
+	 * @return string|int
+	 */
+	public function getTransactionCode()
+	{
+		return $this->_transactionCode;
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see BGI\Row\Object.ObjectInterface::setTransactionCode()
+	 * @return AbstractObject
+	 */
+	public function setTransactionCode($transactionCode)
+	{
+		$this->_transactionCode = $transactionCode;
+		return $this;
+	}
+
+	/**
+	 * Set single datum to a certain value. Datum can for example be
+	 * transactionCode.
+	 * This function should be called by concrete object setData when a datum is
+	 * not specified there.
+	 * @param string $datum
+	 * @param string $value
+	 */
+	private function setDatum($datum, $value)
+	{
+			switch ($datum)
+			{
+				case 'transactionCode':
+					$this->setTransactionCode($value);
+				default:
+					break;
+			}
+
+	}
 }
--- src/BGI/Row/Object/ObjectInterface.php
+++ src/BGI/Row/Object/ObjectInterface.php
@@ -1,6 +1,11 @@
 <?php
 namespace BGI\Row\Object;
 
+/**
+ * This interface declares the methods required for an class to be able to
+ * fulfill the role as an BGI\Row\Object.
+ * @author Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 interface ObjectInterface
 {
 	/**
@@ -14,4 +19,15 @@ interface ObjectInterface
 	 * @param array $data
 	 */
 	public function setData(array $data);
+
+	/**
+	 * Return the transaction code.
+	 */
+	public function getTransactionCode();
+
+	/**
+	 * Set the transaction code.
+	 * @param string $transactionCode
+	 */
+	public function setTransactionCode($transactionCode);
 }
