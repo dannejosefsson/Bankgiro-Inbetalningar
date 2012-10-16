1e75e512ae3fe008ddcd31531be859fd5326d7b9
Daniel Josefsson <dannejosefsson@gmail.com>
2012-10-16 13:36:14 +0200

    Abstract class and interface for parsers added.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- src/BGI/Row/Parser/AbstractParser.php
+++ src/BGI/Row/Parser/AbstractParser.php
@@ -1,36 +1,120 @@
 <?php
+/**
+ * This file contains the AbstractParser.
+ * @license	http://www.gnu.org/licenses/gpl.html
+ * @author		Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 namespace BGI\Row\Parser;
 
-use BGI\Row\Object\ObjectInterface;
+use BGI\Row\Object as Object;
 
+/**
+ * This file contains the AbstractParser.
+ * @license	http://www.gnu.org/licenses/gpl.html
+ * @author		Daniel Josefsson <dannejosefsson@gmail.com>
+ * @abstract
+ * @uses		ParserInterface
+ */
 abstract class AbstractParser implements ParserInterface
 {
+	/**#@+ @access protected */
+
 	/**
-	 * @access	protected
-	 * @var		ObjectInterface
+	 * Valid transaction codes.
+	 * $var array valid transaction codes.
+	 */
+	protected $_validTransactionCodes = array();
+
+	/**
+	 * Instance of an ObjectInterface.
+	 * @var		Object\ObjectInterface
 	 */
 	protected $_objectType = null;
 
 	/**
-	 * (non-PHPdoc)
-	 * @see BGI\Row\Parser.ParserInterface::checkSyntax()
+	 * Current state.
+	 * @var string
 	 */
-	abstract public		function checkSyntax($rowData);
+	protected $_state = self::STATE_IDLE;
+	/**#@- */
+
+	const STATE_IDLE	= 'Idle';
+	const STATE_ERROR	= 'Error occured';
 
 	/**
-	 * (non-PHPdoc)
-	 * @see BGI\Row\Parser.ParserInterface::parseRowData()
+	 * {@inheritdoc}
+	 * @abstract
 	 */
-	abstract public		function parseRowData($rowData);
+	abstract public function checkSyntax($rowData);
 
 	/**
-	 * Factory method to set the return type of parseRowData
-	 * @param ObjectInterface $rowObject
+	 * {@inheritdoc}
+	 * @abstract
 	 */
-	abstract public		function setObjectType(ObjectInterface $rowObject = null);
+	abstract public function parseRow($transactionCode, $rowData);
 
 	/**
-	 * @param array $data
+	 * {@inheritdoc}
+	 * @return	AbstractParser
 	 */
-	abstract protected	function getObject(array $data);
+	public function setObjectType(Object\ObjectInterface $rowObject = null)
+	{
+		$this->_objectType = $rowObject;
+		return $this;
+	}
+
+	/**
+	 * Returns the object and sets it with data. Is a factory method; silently
+	 * gets an object, if neccessary.
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @param	array $data
+	 * @abstract
+	 * @return	Object\ObjectInterface
+	 */
+	abstract protected function getObject(array $data = array());
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public	function getObjectType()
+	{
+		return $this->_objectType;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getState()
+	{
+		return $this->_state;
+	}
+
+	/**
+	 * Checks so the transaction code is an valid one for this parser.
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @param	string $transactionCode
+	 * @uses	AbstractParser::$_validTransactionCodes
+	 * @throws	ParserException
+	 * @return	boolean
+	 */
+	protected function checkTransactionCode($transactionCode)
+	{
+		if (!is_array($this->_validTransactionCodes))
+			throw new
+				ParserException('$_validTransactioncodes must be an array.');
+
+		if (empty($this->_validTransactionCodes))
+			throw new ParserException('$_validTransactioncodes must values.');
+
+		if (!in_array($transactionCode, $this->_validTransactionCodes))
+		{
+			$validTransactionCodes =
+				'Valid transaction codes: ' .
+				implode(', ', $this->_validTransactionCodes).
+				'. '. $transactionCode . ' given.';
+			throw new ParserException( $validTransactionCodes);
+		}
+
+		return true;
+	}
 }
--- src/BGI/Row/Parser/ParserInterface.php
+++ src/BGI/Row/Parser/ParserInterface.php
@@ -1,17 +1,56 @@
 <?php
+/**
+ * This file includes the ParserInterface.
+ * @license	http://www.gnu.org/licenses/gpl.html
+ * @author		Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 namespace BGI\Row\Parser;
 
+use BGI\Row\Object\ObjectInterface;
+
+/**
+ * This file includes the ParserInterface.
+ * @license	http://www.gnu.org/licenses/gpl.html
+ * @author		Daniel Josefsson <dannejosefsson@gmail.com>
+ */
 interface ParserInterface
 {
 	/**
-	 * Make sure that the row data follows the syntax given by the specification.
+	 * Make sure that the row data follows the syntax given by the
+	 * specification.
 	 * Note that the transaction code should not be in the row data.
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
 	 * @param string $rowData
 	 */
 	public function checkSyntax($rowData);
+
 	/**
 	 * Parse the row data into a row object and return false if fails.
-	 * @param string $rowData
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @param	string $transactionCode
+	 * @param	string $rowData
+	 * @return	boolean
 	 */
-	public function parseRowData($rowData);
+	public function parseRow($transactionCode, $rowData);
+
+	/**
+	 * Factory method to set the return type of parseRowData
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @param ObjectInterface $rowObject
+	 */
+	public	function setObjectType(ObjectInterface $rowObject = null);
+
+	/**
+	 * Returns the object
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @return	string
+	 */
+	public	function getObjectType();
+
+	/**
+	 * Returns the object state
+	 * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+	 * @return	string
+	 */
+	public function getState();
 }
